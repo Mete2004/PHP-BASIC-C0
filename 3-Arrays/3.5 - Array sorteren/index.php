13798a84bb3d8edfda461354b4643f977411ccf8
Mete2004 <2150982@talnet.nl>
2022-01-13 16:17:04 +0100

    3.5 gefixt

--- 3-Arrays/3.5 - Array sorteren/index.php
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -5,18 +5,24 @@
 
 
 <?php
-$getallen = array("3","46","78657","47","676","5345","21");
-print_r($getallen);
-
-$hoog_laag = $getallen;
-
-asort($hoog_laag);
+$getallen = array("3","21","46","47","676","5345","78657");
 print_r($getallen);
 
 $laag_hoog = $getallen;
 
-arsort($laag_hoog);
-print_r($getallen);
+asort($laag_hoog);
+echo "<pre>";
+print_r($getallen = $laag_hoog);
+echo "</pre>";
+
+
+$hoog_laag = $getallen;
+
+arsort($hoog_laag);
+echo "<pre>";
+print_r($getallen = $hoog_laag);
+echo "</pre>";
+
 ?>
 
 
